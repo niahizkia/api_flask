dce831931ffb7a0f9af6133680b8a46a6e51160a
niahizkia <niahizkia255@gmail.com>
2022-10-20 21:21:56 +0700

    module 4

--- messages.py
+++ messages.py
@@ -2,3 +2,14 @@ from flask import jsonify
 from flask_restful import Resource, Api
 import models
 
+class MessageList(Resource):
+    def get(self):
+        # get data from database
+        messages = {}
+        query = models.Message.select()
+
+        for row in query:
+            messages[row.id] = {'content': row.content,
+                                'published_at' : row.published_at}
+
+        return jsonify({'messages':messages})
