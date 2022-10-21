a8930f9827cce5c9afdf1518878dfcc1a3b4536b
niahizkia <niahizkia255@gmail.com>
2022-10-21 10:55:59 +0700

    get data by id, reqparse and post data

--- resources/messages.py
+++ resources/messages.py
@@ -1,8 +1,24 @@
 from flask import jsonify, Blueprint
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 import models
 
 class MessageList(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'content',
+            required = True,
+            help     = 'konten wajib ada',
+            location = ['form']
+        )
+        self.reqparse.add_argument(
+            'published_at',
+            required = True,
+            help     = 'published_at/waktu wajib ada',
+            location = ['form']
+        )
+        super().__init__()
+
     def get(self):
         # get data from database
         messages = {}
@@ -11,8 +27,19 @@ class MessageList(Resource):
         for row in query:
             messages[row.id] = {'content': row.content,
                                 'published_at' : row.published_at}
-
         return jsonify({'messages':messages})
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        message = models.Message.create(**args)
+        return jsonify({'success': 'True', 'message':message.content})
+
+
+class Message(Resource):
+
+    def get(self, id):
+        message = models.Message.get_by_id(id)
+        return jsonify({'messages':message.content})
 
 
 messages_api = Blueprint('resources/messages', __name__)
@@ -20,3 +47,4 @@ api          = Api(messages_api)
 
 
 api.add_resource(MessageList, '/messages', endpoint='messages')
+api.add_resource(Message, '/message/<int:id>', endpoint='message')
