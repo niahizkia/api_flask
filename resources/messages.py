2878b1f2ef6c748120f87d1f3396c6e63595ddbe
niahizkia <niahizkia255@gmail.com>
2022-10-21 14:37:04 +0700

    module 7, marshal and marshal_with

--- resources/messages.py
+++ resources/messages.py
@@ -1,6 +1,21 @@
-from flask import jsonify, Blueprint
-from flask_restful import Resource, Api, reqparse
+from flask import jsonify, Blueprint, abort
+from flask_restful import Resource, Api, reqparse, fields, marshal, marshal_with
 import models
+
+message_fields = {
+    'id'            : fields.Integer,
+    'content'       : fields.String,
+    'published_at'  : fields.String
+}
+
+
+def get_or_abort(id):
+    try:
+        msg=models.Message.get_by_id(id)
+    except models.Message.DoesNotExist:
+        abort(404)
+    else:
+        return msg
 
 class MessageList(Resource):
     def __init__(self):
@@ -21,25 +36,22 @@ class MessageList(Resource):
 
     def get(self):
         # get data from database
-        messages = {}
-        query = models.Message.select()
-
-        for row in query:
-            messages[row.id] = {'content': row.content,
-                                'published_at' : row.published_at}
-        return jsonify({'messages':messages})
+        messages = [marshal(message, message_fields) 
+                    for message in models.Message.select()]
+        return {'messages':messages}
 
     def post(self):
         args = self.reqparse.parse_args()
         message = models.Message.create(**args)
-        return jsonify({'success': 'True', 'message':message.content})
+        return marshal(message, message_fields)
 
 
 class Message(Resource):
+    @marshal_with(message_fields)
 
     def get(self, id):
         message = models.Message.get_by_id(id)
-        return jsonify({'messages':message.content})
+        return get_or_abort(id)
 
 
 messages_api = Blueprint('resources/messages', __name__)
